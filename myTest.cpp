13a601b6560ca4ecea8292e2a7ee74c717ef6494
LAgagggggg <15807814960@163.com>
2020-03-07 22:40:19 +0800

    make framework

--- myTest.cpp
+++ myTest.cpp
@@ -1,18 +1,24 @@
 #include "./smartcuckoo.h"
 #include "CoCuckoo.h"
+#include <iostream>
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
     init();
+    CocuckooHashTable * table = cocuckooInit();
 
     auto str = "hhhhh";
     insert(str);
     insert("wtf");
+    cocuckooInsert(*table, str, "wtf");
 
     int result = search("hhhhh");
     printf("%d\n", result);
+
+    DataType * r = cocuckooQuery(*table, str);
+    cout << *r << endl;
 
     return 0;
 }
