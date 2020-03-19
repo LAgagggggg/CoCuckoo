f833ea27b43ed775dcd7ef89f845aacf872701ed
lagagggggg <lagagggggg@gmail.com>
2020-03-19 17:02:32 +0800

    implement simple concurrent test

--- ycsb.cpp
+++ ycsb.cpp
@@ -21,13 +21,16 @@ typedef struct sub_thread{
     thread_queue* run_queue;
 } sub_thread;
 
+void printTable(CocuckooHashTable &table);
+
 void ycsb_thread_run(void* arg){
     sub_thread* subthread = (sub_thread *)arg;
     int i = 0;
     printf("Thread %d is opened\n", subthread->id);
+    string value = "insert by thread " + to_string(subthread->id) ;
     for(; i < READ_WRITE_NUM/subthread->table->thread_num; i++){
         if( subthread->run_queue[i].operation == 1){
-            if (cocuckooInsert(*(subthread->table), subthread->run_queue[i].key, subthread->run_queue[i].key)==0){   
+            if (cocuckooInsert(*(subthread->table), subthread->run_queue[i].key, value)==0){   
                 subthread->inserted ++;
             }
         }else{
@@ -46,6 +49,7 @@ void ycsb_thread_run(void* arg){
 int main(int argc, char* argv[])
 {        
     int thread_num = 4;             // INPUT: the number of threads
+    bool readFromFile = false;
 
     CocuckooHashTable &table = *cocuckooInit();
     table.thread_num = thread_num;
@@ -58,64 +62,77 @@ int main(int argc, char* argv[])
     struct timespec start, finish;
     double single_time;
 
-    if((ycsb = fopen("./workloads/rw-50-50-load.txt","r")) == NULL)
-    {
-        perror("fail to read");
-    }
-
-    printf("Load phase begins \n");
-	while(getline(&buf,&len,ycsb) != -1){
-		if(strncmp(buf, "INSERT", 6) == 0){
-			memcpy(&key, buf+7, KEY_LEN-1);
-            string str = to_string(key);
-			if (cocuckooInsert(table, str, str)==0)                      
-			{
-				inserted ++;
-			}
-			else{
-				break;
-			}
-		}
-	}
-	fclose(ycsb);
-    printf("Load phase finishes: %llu items are inserted \n", inserted);
-
-    if((ycsb_read = fopen("./workloads/rw-50-50-run.txt","r")) == NULL)
-    {
-        perror("fail to read");
-    }
-
     thread_queue* run_queue[thread_num];
     int move[thread_num];
+    int operation_num = 0;	
     for(t = 0; t < thread_num; t ++){
         run_queue[t] =  (thread_queue *)calloc(READ_WRITE_NUM/thread_num, sizeof(thread_queue));
         move[t] = 0;
     }
 
-	int operation_num = 0;		
-	while(getline(&buf,&len,ycsb_read) != -1){
-		if(strncmp(buf, "INSERT", 6) == 0){
-            memcpy(&key, buf+7, KEY_LEN-1);
-            string str = to_string(key);
-            run_queue[operation_num%thread_num][move[operation_num%thread_num]].key = str;
-			run_queue[operation_num%thread_num][move[operation_num%thread_num]].operation = 1;
-			move[operation_num%thread_num] ++;
-		}
-		else if(strncmp(buf, "READ", 4) == 0){
-            memcpy(&key, buf+5, KEY_LEN-1);
-            string str = to_string(key);
-            run_queue[operation_num%thread_num][move[operation_num%thread_num]].key = str;
-			run_queue[operation_num%thread_num][move[operation_num%thread_num]].operation = 0;
-			move[operation_num%thread_num] ++;
-		}
-		operation_num ++;
-	}
-	fclose(ycsb_read);
+
+    if (readFromFile)
+    {
+        if((ycsb = fopen("./workloads/rw-50-50-load.txt","r")) == NULL)
+        {
+            perror("fail to read");
+        }
+
+        printf("Load phase begins \n");
+        while(getline(&buf,&len,ycsb) != -1){
+            if(strncmp(buf, "INSERT", 6) == 0){
+                string str = buf + 7;
+                if (cocuckooInsert(table, str, str)==0)                      
+                {
+                    inserted ++;
+                }
+                else{
+                    break;
+                }
+            }
+        }
+        fclose(ycsb);
+        printf("Load phase finishes: %lu items are inserted \n", inserted);
+
+        if((ycsb_read = fopen("./workloads/rw-50-50-run.txt","r")) == NULL)
+        {
+            perror("fail to read");
+        }
+	
+        while(getline(&buf,&len,ycsb_read) != -1){
+            if(strncmp(buf, "INSERT", 6) == 0){
+                string str = buf + 7;
+                run_queue[operation_num%thread_num][move[operation_num%thread_num]].key = str;
+                run_queue[operation_num%thread_num][move[operation_num%thread_num]].operation = 1;
+                move[operation_num%thread_num] ++;
+            }
+            else if(strncmp(buf, "READ", 4) == 0){
+                string str = buf + 5;
+                run_queue[operation_num%thread_num][move[operation_num%thread_num]].key = str;
+                run_queue[operation_num%thread_num][move[operation_num%thread_num]].operation = 0;
+                move[operation_num%thread_num] ++;
+            }
+            operation_num ++;
+        }
+        fclose(ycsb_read);
+    }
+    else {
+        //insert into CoCuckoo
+        for (int i = 0; i < 100; i++)
+        {
+            string s = "test";
+            s += to_string(i);
+            run_queue[operation_num%thread_num][move[operation_num%thread_num]].key = s;
+            run_queue[operation_num%thread_num][move[operation_num%thread_num]].operation = 1;
+            move[operation_num%thread_num] ++;
+            operation_num ++;
+        }
+    }
 
 	sub_thread* THREADS = (sub_thread*)malloc(sizeof(sub_thread)*thread_num);
     inserted = 0;
 	
-	// printf("Run phase begins: SEARCH/INSERTION ratio is 50\%/50\% \n");
+	printf("Run phase begins:\n");
     clock_gettime(CLOCK_MONOTONIC, &start);	
     for(t = 0; t < thread_num; t++){
         THREADS[t].id = t;
@@ -135,9 +152,39 @@ int main(int argc, char* argv[])
     for(t = 0; t < thread_num; ++t){
         inserted +=  THREADS[t].inserted;
     }
-    printf("Run phase finishes: %llu/%llu items are inserted/searched\n", operation_num - inserted, inserted);
+    printf("Run phase finishes: %lu/%lu items are inserted/searched\n", operation_num - inserted, inserted);
     printf("Run phase throughput: %f operations per second \n", READ_WRITE_NUM/single_time);	
+    
+    //check
+    printTable(table);
+    for (int i = 0; i < inserted; i++)
+    {
+        string s = "test";
+        s += to_string(i);
+        auto result = cocuckooQuery(table, s);
+        if (result == NULL) {
+            printf("\"%s\" not exist\n", s.c_str());
+        }
+    }
     
     return 0;
 }
 
+void printTable(CocuckooHashTable &table) {
+    int i = 0;
+    while (i<table.size)
+    {
+        if (i % 8 == 0){
+            putchar('\n');
+        }
+        if (table.data[i].occupied)
+        {
+            printf("%3d: %10.10s |", i, table.data[i].key.c_str());
+        }
+        else {
+            printf("%3d: %10s |", i, "");
+        }
+        i++;
+    }
+    putchar('\n');
+}
